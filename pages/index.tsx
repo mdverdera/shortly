825845bce4d06678889059f51c1e91e9213983a7
MadMan24 <mdverdera@yahoo.com>
2022-09-22 07:56:21 +0800

    Create website landing page

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,7 +7,7 @@ const Home: NextPage = () => {
   return (
     <>
       <Head>
-        <title>My page title</title>
+        <title>Shortly</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         <link
           rel="shortcut icon"
@@ -64,6 +64,36 @@ const Home: NextPage = () => {
 
         {/* Mobile Menu */}
       </nav>
+
+      {/* Hero Section */}
+      <section id="hero">
+        {/* Hero Container */}
+        <div className="container flex flex-col-reverse mx-auto p-6 lg:flex-row">
+          {/* Content Container */}
+          <div className="flex flex-col space-y-10 mb-44 lg:mt-16 lg:w-1/2 xl:mb-52">
+            <h1 className="text-5xl font-bold text-center lg:text-6xl lg:max-w-md lg:text-left">
+              More than just shorter links
+            </h1>
+            <p className="text-2xl text-center text-gray-400 lg:max-w-md lg:text-left">
+              Build your brand's recognition and get detailed insights on how
+              your links are performing.
+            </p>
+            <div className="mx-auto lg:mx-0">
+              <Link href="#">
+                <a className="py-5 px-10 text-2xl font-bold text-white bg-cyan rounded-full lg:py-4 hover:opacity-70">
+                  Get Started
+                </a>
+              </Link>
+            </div>
+          </div>
+          {/* Image */}
+          <div className="mb-24 mx-auto md:w-180 lg:mb-0 lg:w-1/2">
+            <picture>
+              <img src="/img/illustration-working.svg" alt="working" />
+            </picture>
+          </div>
+        </div>
+      </section>
     </>
   );
 };
